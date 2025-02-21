ef3cc0416cfd927628d3f16d42949d035e59d146
beccapana <88428390+beccapana@users.noreply.github.com>
2025-02-21 15:56:31 +0300

    блять, не пофиксил. А тут хз как

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-import tests
-import psutil
-import time
 from tests.m_tests import *
-from config import*
+from config import *
 
 run_bat_and_wait_for_output("run_yuma.bat", "Entering main loop.")
 
